f677a90645a5614c5254a2c4c213b19cbeb3fed0
Snehasish Paul <weishenpov@gmail.com>
2024-10-08 02:55:08 +0530

    modification done & tested app with ping
    modification done & tested app with ping

--- src/main/java/com/snehasish/microservice/quiz/QuizAppQuizMicroServiceApplication.java
+++ src/main/java/com/snehasish/microservice/quiz/QuizAppQuizMicroServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuizApplicationQuizMicroServiceApplication {
+public class QuizAppQuizMicroServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuizApplicationQuizMicroServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuizAppQuizMicroServiceApplication.class, args);
+    }
 
 }
