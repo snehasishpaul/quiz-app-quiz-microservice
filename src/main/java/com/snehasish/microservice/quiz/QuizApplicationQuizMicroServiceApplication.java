2ea16778e2007d51360e0f12d7cbd0164cca5414
Snehasish Paul <weishenpov@gmail.com>
2024-10-08 02:28:19 +0530

    initial commit
    initial commit

---
+++ src/main/java/com/snehasish/microservice/quiz/QuizApplicationQuizMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.snehasish.microservice.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApplicationQuizMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizApplicationQuizMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snehasish/microservice/quiz/QuizApplicationQuizMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snehasish.microservice.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationQuizMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
